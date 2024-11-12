0c5a35a45e8815dbd64b0d515a580a6c18bc0d18
W1llAnn <9506514719@mail.ru>
2024-11-12 13:14:25 +0500

    commit_1

---
+++ app/model.py
@@ -0,0 +1,45 @@
+import torch
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+
+
+colors = {
+    0: (36, 36, 36),
+    1: (200, 200, 200),
+    2: (90, 77, 227),
+    3: (206, 214, 156),
+    4: (133, 173, 130),
+    5: (207, 136, 37),
+    6: (122, 2, 16)
+}
+
+def load_model(model_path: str):
+    model = torch.load(model_path, map_location=torch.device('cpu'))
+    model.eval()  # Установка модели в режим оценки
+    return model
+
+def segment_image(model, image):
+    # Преобразование изображения для модели
+    preprocess = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    input_tensor = preprocess(image)
+    input_batch = input_tensor.unsqueeze(0)  # Добавляем размерность для батча
+
+    with torch.no_grad():
+        output = model(input_batch)  # Получаем выходные данные модели
+
+    # Получаем предсказанную маску
+    predicted_mask = torch.argmax(output, dim=1).cpu().numpy()[0]
+
+    # Создание цветной маски на основе предсказанной маски
+    colored_mask = np.zeros((*predicted_mask.shape, 3), dtype=np.uint8)
+    
+    for cls, color in colors.items():
+        colored_mask[predicted_mask == cls] = color
+
+    return Image.fromarray(colored_mask)  # Конвертация обратно в изображение PIL
---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from PIL import Image
+import io
+import torch
+from app.model import load_model, segment_image
+
+app = FastAPI()
+
+# Загрузка модели
+model = load_model("app/model_3_8.pt")
+
+@app.post("/segment/")
+async def segment(file: UploadFile = File(...)):
+    # Чтение изображения из загруженного файла
+    image = Image.open(file.file).convert("RGB")
+    
+    # Сегментация изображения
+    segmented_image = segment_image(model, image)
+    
+    # Конвертация сегментированного изображения в байты для ответа
+    img_byte_arr = io.BytesIO()
+    segmented_image.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+    
+    return StreamingResponse(img_byte_arr, media_type="image/png")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=1024)
